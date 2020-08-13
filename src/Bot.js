40018dcc2211a0bbc837a20ca94de711ae052542
henry-lang <Henry.langmack@gmail.com>
2020-08-13 11:13:33 -0400

    Image url checking

--- src/Bot.js
+++ src/Bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const Firebase = require('firebase');
 const Config = require('./Config.json');
+const IsImageUrl = require('is-image-url');
+const { syncBuiltinESMExports } = require('module');
 
 var bot = new Discord.Client();
 var cmds = {
@@ -11,10 +13,18 @@ var cmds = {
             let url = args[0];
             let category = args[1];
 
-            memes.child(category).push({
-                url: url,
-                score: 0
-            });
+            if(IsImageUrl(url)) {
+                message.channel.send("Image is valid url.");
+            } else {
+                message.channel.send(":x: The url you sent is not an image.").then(function(msg){
+                    msg.delete({timeout: 1000});
+                });
+                message.delete({timeout: 1000});
+            }
+            // memes.child(category).push({
+            //     url: url,
+            //     score: 0
+            // });
         }
     }
 }
@@ -25,20 +35,20 @@ var memes = Firebase.database().ref('memes');
 bot.once('ready', function() {
     console.log('Bot ready!');
     bot.on('message', function(message) {
-        if(message.content.startsWith(Config.prefix)) {
-            let cmd = message.content.split(' ')[0].substr(1).toLowerCase();
-            let args = message.content.split(' ');
-            args.shift();
+        if(!message.content.startsWith(Config.prefix) || message.author.bot) return;
 
-            if(Object.keys(cmds).includes(cmd)) {
-                if(args.length != cmds[cmd].args) {
-                    message.channel.send('Incorrect usage. Usage: ' + cmds[cmd].usage);
-                } else {
-                    cmds[cmd].func(message, args);
-                }
+        let cmd = message.content.split(' ')[0].substr(1).toLowerCase();
+        let args = message.content.split(' ');
+        args.shift();
+
+        if(Object.keys(cmds).includes(cmd)) {
+            if(args.length != cmds[cmd].args) {
+                message.channel.send('Incorrect usage. Usage: ' + cmds[cmd].usage);
             } else {
-                message.channel.send('Unknown command.');
+                cmds[cmd].func(message, args);
             }
+        } else {
+            message.channel.send('Unknown command.');
         }
     });
 });
